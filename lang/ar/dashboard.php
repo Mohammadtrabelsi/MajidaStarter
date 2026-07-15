67c84068b2977507ea5788f102f9e58be80dfdf9
Mohammad Trabelsi <mohammad.trabelsi@gmail.com>
2026-07-15 08:21:51 +0100

    Add Arabic and French language files for app and dashboard translations

---
+++ lang/ar/dashboard.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'account' => 'الحساب',
+    'admin_note' => 'ملاحظة المسؤول',
+    'admin_panel' => 'لوحة المسؤول',
+    'administrator' => 'المدير',
+    'email' => 'البريد الإلكتروني',
+    'member' => 'العضو',
+    'member_note' => 'ملاحظة العضو',
+    'member_since' => 'عضو منذ',
+    'name' => 'الاسم',
+    'role' => 'الدور',
+    'title' => 'العنوان',
+    'welcome_back' => 'مرحباً بعودتك، :name',
+];
---
+++ lang/fr/dashboard.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'account' => 'Compte',
+    'admin_note' => 'Note de l\'administrateur',
+    'admin_panel' => 'Panneau d\'administration',
+    'administrator' => 'Administrateur',
+    'email' => 'E-mail',
+    'member' => 'Membre',
+    'member_note' => 'Note du membre',
+    'member_since' => 'Membre depuis',
+    'name' => 'Nom',
+    'role' => 'Rôle',
+    'title' => 'Titre',
+    'welcome_back' => 'Bon retour, :name',
+];
